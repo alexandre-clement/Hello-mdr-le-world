74a3b2fc5e89475ecf87197beaa700643cd45a4b
alexandre-clement <alexandre.didier.clement@gmail.com>
2016-10-05 18:44:50 +0200

    edit main again 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,7 +16,7 @@ import interpreter.Interpreter;
 
 public class Main {
     public static void main(String... args) {
-        System.out.println("edit3");
+        System.out.println("edit4");
         Interpreter interpreter = new Interpreter(args);
         interpreter.buildSystem();
     }
