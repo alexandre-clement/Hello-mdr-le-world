f92ce8ac8a0236cfd836b6655a3810080834940a
alexandre-clement <alexandre.didier.clement@gmail.com>
2016-11-11 00:08:24 +0100

    Interpreter improvement.
    use lib org.apache.commons.cli

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         int exit = 0;
         try {
-            new Interpreter().build(args).;
+            new Interpreter().build(args);
         } catch (ExitException exception) {
             System.err.println(exception.getMessage());
             exit = exception.getExit();
