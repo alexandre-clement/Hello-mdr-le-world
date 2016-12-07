e1a1ea467a182d788060ce87b62be2a236d8aac0
alexandre-clement <alexandre.didier.clement@gmail.com>
2016-12-07 15:05:15 +0100

    Ajout des répertoires de tests

---
+++ src/test/java/interpreter/InterpreterTest.java
@@ -0,0 +1,48 @@
+package interpreter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Alexandre Clement
+ *         Created the 07/12/2016.
+ */
+public class InterpreterTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void buildInterpreter() throws Exception {
+
+    }
+
+    @Test
+    public void getOptionValue() throws Exception {
+
+    }
+
+    @Test
+    public void hasOption() throws Exception {
+
+    }
+
+    @Test
+    public void getOptions() throws Exception {
+
+    }
+
+    @Test
+    public void help() throws Exception {
+
+    }
+
+    @Test
+    public void version() throws Exception {
+
+    }
+
+}
